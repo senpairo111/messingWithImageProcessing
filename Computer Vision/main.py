41694294e463013fdca51a8e35e09320a0b579c5
senpairo111 <74233627+senpairo111@users.noreply.github.com>
2021-10-12 10:11:25 +0300

    Update main.py

--- Computer Vision/main.py
+++ Computer Vision/main.py
@@ -6,8 +6,7 @@ threshold = gbv.ColorThreshold([[17, 27], [95, 175], [99, 179]], 'HSV') + gbv.Me
 
 pipe = threshold + gbv.find_contours + gbv.FilterContours(
     100) + gbv.contours_to_rotated_rects_sorted + gbv.filter_inner_rotated_rects
-
-JOY_CON = gbv.GameObject(0.05916079783099616)
+TARGET = gbv.GameObject(0.05916079783099616)
 
 
 def main():
@@ -24,8 +23,8 @@ def main():
         if len(cnts) > 0:
             root = gbv.BaseRotatedRect.shape_root_area(cnts[0])
             center = gbv.BaseRotatedRect.shape_center(cnts[0])
-            print(JOY_CON.distance_by_params(cam, root))
-            print(JOY_CON.location_by_params(cam, root, center))
+            print(TARGET.distance_by_params(cam, root))
+            print(TARGET.location_by_params(cam, root, center))
 
 
 if __name__ == '__main__':
