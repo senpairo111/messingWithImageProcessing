7664d8fa3ffa6c30c38315b3476e532a48a1e3bb
senpairo111 <nadavh2611@gmail.com>
2022-04-29 11:42:26 +0300

    now PID and not PI
    not calibrated at all how TF DOES IT WORK SO WELL!!!!!!

--- Computer Vision/adaptive_vision.py
+++ Computer Vision/adaptive_vision.py
@@ -16,14 +16,19 @@ sock = socket.socket
 def main():
     cam = gbv.USBCamera(settings.CAMERA_PORT, gbv.LIFECAM_3000) 
     cam.set_exposure(settings.EXPOSURE)
+    last_exposure = settings.EXPOSURE
     exposure_integral = 0
     ok, frame = cam.read()
     win = gbv.FeedWindow("window")
     thr = gbv.FeedWindow("threshold")
     raw = gbv.FeedWindow("raw")
+    bbox = None
     while win.show_frame(frame):
+        slight_denoise = gbv.MedianBlur(7)
+        frame = slight_denoise(frame)
+        
         # where we adapt
-        cur_thr = gbv.median_threshold(frame, [0, 0, 0], None, 'HSV')
+        cur_thr = gbv.median_threshold(frame, [0, 0, 0], bbox, 'HSV')
         hue = default_vals[0] 
         sat = default_vals[1] 
         val = default_vals[2] 
@@ -33,23 +38,30 @@ def main():
         range_val = default_range[2] 
         
         print(cur_thr.__getitem__(2)[0])
+        
+        # exposure PID
         exposure_error = (val - cur_thr.__getitem__(2)[0])
         exposure_integral += exposure_error
-        cam.set_exposure(settings.EXPOSURE + exposure_error * settings.EXPOSURE_KP + exposure_integral * settings.EXPOSURE_KI)
+        exposure_derivative = last_exposure - exposure_error
+        cam.set_exposure(-(exposure_error * settings.EXPOSURE_KP
+                           ) + (exposure_integral * settings.EXPOSURE_KI)
+                         - (exposure_derivative * settings.EXPOSURE_KD))
+        last_exposure = exposure_error
         
         final_thr.__setitem__(0, [hue - range_hue, hue + range_hue])
         final_thr.__setitem__(1, [sat - range_sat, sat + range_sat])
         final_thr.__setitem__(2, [val - range_val, val + range_val])
         
-        threshold =  final_thr + gbv.MedianBlur(3) + gbv.Dilate(12, 9
-            )  + gbv.Erode(5, 9) + gbv.DistanceTransformThreshold(0.3)
+        # threshold
+        threshold =  final_thr + gbv.Dilate(13, 3
+            )  + gbv.Erode(5, 2) + gbv.DistanceTransformThreshold(0.3)
 
-        
+        # pipe
         pipe = threshold + gbv.find_contours + gbv.FilterContours(
             100) + gbv.contours_to_rotated_rects_sorted + gbv.filter_inner_rotated_rects
         ok, frame = cam.read()
-        thr.show_frame(threshold(frame))
-        raw.show_frame(final_thr(frame))
+        #thr.show_frame(threshold(frame))
+        #raw.show_frame(final_thr(frame))
         cnts = pipe(frame)
         frame = gbv.draw_rotated_rects(frame, cnts, (255, 0, 0), thickness=5)
         if len(cnts) > 0:
@@ -60,10 +72,10 @@ def main():
             print("location:" + str(locals))
             print("angle:" + str(np.arcsin(locals[0] / locals[2]) * 180 / np.pi))
             
-            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as sock:
-                sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-                sock.sendto(struct.pack('ddd', locals[0], locals[1], locals[2]),
-                    ("255.255.255.255", 5162))
+            # with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as sock:
+            #     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            #     sock.sendto(struct.pack('ddd', locals[0], locals[1], locals[2]),
+            #         ("255.255.255.255", 5162))
                 
                 
 
